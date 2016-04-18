a43d9b49c02393976002ded5119a5cdfff5bc481
Troshuk <Troshuk@gmail.com>
2016-04-18 18:36:48 +0300

    Tank 1.0

---
+++ NewProject/Tank/src/Tank/Game.java
@@ -0,0 +1,10 @@
+package Tank;
+
+/**
+ * Created by Progr@mist on 18.04.2016.
+ */
+public class Game {
+    String name;
+    int life;
+    int
+}
---
+++ NewProject/Tank/src/Tank/Tank.java
@@ -0,0 +1,9 @@
+package Tank;
+
+/**
+ * Created by Progr@mist on 18.04.2016.
+ */
+public class Tank {
+
+
+}
